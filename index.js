2595ff1acccafc1db4d7906f260ae3eb090f1975
Matt Holmes <matt.php@aceviral.com>
2016-08-02 11:51:44 +0100

    hacky update for windows support

--- index.js
+++ index.js
@@ -4,68 +4,96 @@ var os = require("os");
 
 var LINUX_EXIT_REG = /^(\d+) packets transmitted, (\d+) received, (.*)% packet loss, time (\d+)ms$/m;
 var MAC_EXIT_REG = /^(\d+) packets transmitted, (\d+) packets received, (.*)% packet loss$/m;
+var WIN_EXIT_REG = /Packets: Sent = (\d+), Received = (\d+), Lost = (\d+) \((\d+)% loss\).*?Minimum = \d+ms, Maximum = \d+ms, Average = (\d+)ms/;
 var LINE_REG = /^(\d+) bytes from (.*?): icmp_[rs]eq=(\d+) ttl=(\d+) time=([\d\.]+) ms$/;
+var LINE_WIN = /^Reply from (.*?): bytes=(\d+) time=([\d\.]+)ms TTL=(\d+)$/;
 
 module.exports = function (target, options) {
+  console.log("called")
   var emitter = new events.EventEmitter;
   var packets = 0;
-  var startTime = process.hrtime();
+  var totalTime = 0;
 
 
   options = options || {};
   options.count = options.count || 10;
 
   if (os.platform() == "win32") {
-    var spawn = child.spawn("ping", ["-n", options.count, target]);
+    var spawn = child.spawn("ping", ["/n", options.count, target]);
   } else {
     var spawn = child.spawn("ping", ["-c", options.count, target]);
   }
   spawn.stdout.on("data", data);
 
-  return emitter;
-
   function line(str) {
     str = str.trim().replace(/\s+/g, " ");
 
     var match = str.match(LINE_REG);
-    if (!match) {
-      match_linux = str.match(LINUX_EXIT_REG);
-      match_mac = str.match(MAC_EXIT_REG);
-      if (match_linux) {
-        emitter.emit("exit", {
+    var win_match = str.match(LINE_WIN);
+    if (match) {
+      emitter.emit(
+        "data", {
           target: target,
-          sent: +match_linux[1],
-          recieved: +match_linux[2],
-          loss: +match_linux[3],
-          time: +match_linux[4]
-        });
-      } else if (match_mac) {
-        emitter.emit("exit", {
+          no: ++packets,
+          bytes: +match[1],
+          time: +match[5],
+          ttl: +match[4]
+        }
+      );
+      totalTime += parseFloat(match[5]);
+    } else if (win_match) {
+      emitter.emit(
+        "data", {
           target: target,
-          sent: +match_mac[1],
-          recieved: +match_mac[2],
-          loss: +match_mac[3],
-          time: +process.hrtime(startTime)[0] + " s"
-        });
-      }
+          no: ++packets,
+          bytes: +win_match[2],
+          time: +win_match[3],
+          ttl: +win_match[4]
+        }
+      );
+      totalTime += parseFloat(win_match[3]);
     } else {
-      emitter.emit("data", {
-        target: target,
-        no: ++packets,
-        bytes: +match[1],
-        time: +match[5],
-        ttl: +match[4]
-      });
+      var match_linux = str.match(LINUX_EXIT_REG);
+      var match_mac = str.match(MAC_EXIT_REG);
+      var match_win = str.match(WIN_EXIT_REG);
+      if (match_linux) {
+        emitter.emit(
+          "exit", {
+            target: target,
+            sent: +match_linux[1],
+            recieved: +match_linux[2],
+            loss: +match_linux[3],
+            time: +match_linux[4]
+          }
+        );
+      } else if (match_mac) {
+        emitter.emit(
+          "exit", {
+            target: target,
+            sent: +match_mac[1],
+            recieved: +match_mac[2],
+            loss: +match_mac[3],
+            time: +totalTime
+          }
+        );
+      } else if (match_win) {
+        emitter.emit(
+          "exit", {
+            target: target,
+            sent: +match_win[1],
+            recieved: +match_win[2],
+            loss: +match_win[4],
+            time: +totalTime
+          }
+        );
+      }
     }
   }
 
   function data(str) {
     str = str + "";
-    var lines = str.split("\n");
-    if (lines.length > 1) {
-      lines.forEach(line);
-    } else {
-      line(data);
-    }
+    line(str);
   }
+
+  return emitter;
 };
